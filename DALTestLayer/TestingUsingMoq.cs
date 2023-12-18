010bb8f4eab802b9a57ff1aecbf4aa52582b0eff
Vikramvicky00 <gonthinavikram811@gmail.com>
2023-12-18 18:16:42 +0530

    Add project files.

---
+++ DALTestLayer/TestingUsingMoq.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer;
+using Moq;
+using NUnit.Framework;
+
+namespace DALTestLayer
+{
+    [TestFixture]  
+    public class TestingUsingMoq
+    {
+        [Test]
+        public void checkEmployeeExistWithMoq()
+        {
+        
+            var fakeObject = new Mock<EmpInfoRepository>();
+            fakeObject.Setup(x => x.GetEmpInfo(It.IsAny<string>()));
+            var res = fakeObject.Object.GetEmpInfo("demo@gmail.com");
+            Assert.That(res,Is.Not.Null);
+        }
+    }
+}
